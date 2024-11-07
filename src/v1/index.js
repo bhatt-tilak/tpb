ab89d634c804d3f30a7aae9365a1cf9c4231c5f2
Tilak Bhatt <tilakprasadbhatt@gmail.com>
2024-11-08 01:39:09 +0545

    packages updated

---
+++ src/v1/index.js
@@ -0,0 +1,22 @@
+
+import './styles.css'
+
+const VersionOne = () => {
+
+    return (
+        <div className="main-container">
+            
+            <div className="section-left">
+
+                <h1>Tilak Bhatt</h1>
+                <h2>Frontend Developer</h2>
+            </div>
+
+            <div className="section-right">
+                
+            </div>
+        </div>
+    )
+}
+
+export default VersionOne;
