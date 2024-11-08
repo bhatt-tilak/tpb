fda636a742eb193fabbff393af0b9d19016f592e
Tilak Bhatt <tilakprasadbhatt@gmail.com>
2024-11-08 12:06:41 +0545

    demo home page

---
+++ src/v1/index.tsx
@@ -0,0 +1,17 @@
+
+
+import Home from './pages/Home';
+import './styles.scss'
+
+const VersionOne = () => {
+
+    return (
+        <div className="v1-main-container">
+            {/* Later Add Router Here for navigation purpose */}
+            {/* ok */}
+            <Home />
+        </div>
+    )
+}
+
+export default VersionOne;
